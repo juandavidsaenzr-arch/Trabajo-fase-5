47ec2ac5366d4520b5bc5793eddf7c01ce4294ba
Juan Saenz <juandavidsaenzr@gmail.com>
2026-05-23 14:27:00 -0500

    Creacion de matriz

--- Problema3.py
+++ Problema3.py
@@ -3,3 +3,13 @@
 # Ingenieria de sistemas
 # Fundamentos de programacion 
 # Codigo fuente: Autoria propia
+
+# Matriz del inventario
+inventario = [
+    ["A101", "Teclado", 3, 10],
+    ["A102", "Mouse", 12, 10],
+    ["A103", "Monitor", 2, 5],
+    ["A104", "USB", 20, 15],
+    ["A105", "Impresora", 1, 4]
+]
+
